3b0dd5b34f6bac3314334a3975d3d5adbe0badf8
staticboards <javier@staticboards.com>
2016-03-07 13:58:01 -0500

    enable google analytics

--- header.php
+++ header.php
@@ -9,6 +9,17 @@
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
   <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
 
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-58360056-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+
   <?php wp_head(); ?>
   
 </head>
