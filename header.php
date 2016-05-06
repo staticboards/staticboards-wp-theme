235dd18f0751fa3051abc4cfb9211bc4cdf3bc7b
staticboards <javier@staticboards.com>
2016-05-06 03:48:57 -0400

    header!!!

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+<head>
 
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
