bcc5fd96ec6b1220277855a8a87117933d7f4904
derethor <derethor@gmail.com>
2016-11-03 07:02:44 -0500

    3 columns

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -28,12 +28,14 @@ remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_l
 //add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
 
 remove_action( 'woocommerce_before_shop_loop_item_title', 	'woocommerce_show_product_loop_sale_flash', 		10 );
-remove_action( 'woocommerce_before_main_content', 	'woocommerce_breadcrumb', 					20, 0 )
+remove_action( 'woocommerce_before_main_content', 	'woocommerce_breadcrumb', 					20, 0 );
 
-// ROWS PER COLUMN
 add_filter('loop_shop_columns', 'loop_columns');
 if (!function_exists('loop_columns')) {
-  function staticboards_woocommerce_loop_columns() {return 4;}
+	function loop_columns() {
+		return 3; // 3 products per row
+	}
 }
+
 
 ?>
--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -21,14 +21,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $product, $woocommerce_loop;
 
-// Store loop count we're currently on
-if ( empty( $woocommerce_loop['loop'] ) ) {
-	$woocommerce_loop['loop'] = 0;
-}
-
 // Store column count for displaying the grid
 if ( empty( $woocommerce_loop['columns'] ) ) {
-	$woocommerce_loop['columns'] = apply_filters( 'loop_shop_columns', 4 );
+	$woocommerce_loop['columns'] = apply_filters( 'loop_shop_columns', 3 );
 }
 
 // Ensure visibility
@@ -36,17 +31,6 @@ if ( ! $product || ! $product->is_visible() ) {
 	return;
 }
 
-// Increase loop count
-$woocommerce_loop['loop']++;
-
-// Extra post classes
-$classes = array();
-if ( 0 === ( $woocommerce_loop['loop'] - 1 ) % $woocommerce_loop['columns'] || 1 === $woocommerce_loop['columns'] ) {
-	$classes[] = 'first';
-}
-if ( 0 === $woocommerce_loop['loop'] % $woocommerce_loop['columns'] ) {
-	$classes[] = 'last';
-}
 ?>
 <li <?php post_class( $classes ); ?>>
 
